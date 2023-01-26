39cf18eed59e5f187d9709da6c63f226355e98b3
火火clover <hicug@qq.com>
2023-01-26 19:58:32 +0800

    fix: fixed query param optional

--- src/services/single.ts
+++ src/services/single.ts
@@ -2,17 +2,17 @@ import {BaseType, InputData, Query, Response} from "../types";
 import {strapiRequest} from "./request";
 import qs from "qs";
 
-function get<T extends BaseType = BaseType>(type: string, query: Query<T>): Promise<Response<T>> {
+function get<T extends BaseType = BaseType>(type: string, query?: Query<T>): Promise<Response<T>> {
     return strapiRequest.get(`/${type}?${qs.stringify(query, {encodeValuesOnly: true})}`)
         .then(r => r.data)
 }
 
-function put<T extends BaseType = BaseType>(type: string, data: InputData<T>, query: Query<T>): Promise<Response<T>> {
+function put<T extends BaseType = BaseType>(type: string, data: InputData<T>, query?: Query<T>): Promise<Response<T>> {
     return strapiRequest.put(`/${type}?${qs.stringify(query, {encodeValuesOnly: true})}`,
         {data}).then(r => r.data)
 }
 
-function remove<T extends BaseType = BaseType>(type: string, query: Query<T>): Promise<Response<T>> {
+function remove<T extends BaseType = BaseType>(type: string, query?: Query<T>): Promise<Response<T>> {
     return strapiRequest.delete(`/${type}?${qs.stringify(query, {encodeValuesOnly: true})}`)
         .then(r => r.data)
 }
