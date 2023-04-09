e4a3216f1bc570ea75e3b27e917f3d2945dafa0c
火火clover <hicug@qq.com>
2023-04-09 22:14:12 +0800

    updated: add query param of get me api

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,8 +1,9 @@
-import {UserAttrs, UserLogged} from "../types";
+import {Query, User, UserAttrs, UserLogged} from "../types";
 import {strapiRequest} from "./request";
+import qs from "qs";
 
-export const _me = (strapiRequest) => <T = UserAttrs>(): UserLogged<T>["user"] => {
-    return strapiRequest.get("/users/me")
+export const _me = (strapiRequest) => <T = UserAttrs>(query?: Query<User<T>>): UserLogged<T>["user"] => {
+    return strapiRequest.get(`/users/me?${qs.stringify(query, {encodeValuesOnly: true})}`)
         .then(r => r.data)
 }
 
--- test/test.js
+++ test/test.js
@@ -1,27 +1,11 @@
-const {createStrapiClient} =require("../dist");
+const {createStrapiClient} = require("../dist");
 
-const {strapiClient,user,auth}=createStrapiClient()
+const {strapiClient, user, auth} = createStrapiClient()
 
-auth.login({identifier:"littlebadbad",password:"123456"})
-.then(r=>{
-    strapiClient.interceptors.request.use(config=>{
-        config.headers["authorization"] = `Bearer ${r.jwt}`;
-        return config
-    })
-    return user.me()
-}).then(r=>{
-    //{
-    //   "id": 1,
-    //   "username": "littlebadbad",
-    //   "email": "littlebadbad@qq.com",
-    //   "provider": "local",
-    //   "confirmed": true,
-    //   "blocked": false,
-    //   "createdAt": "2023-03-19T04:59:03.199Z",
-    //   "updatedAt": "2023-04-09T07:51:53.183Z",
-    //   "name": "littlebadbad",
-    //   "bio": "# bio test\n\n1234567\n\n## bio test\n\n1234567\n7654321\n\n### bio test\n\n1234567890"
-    // }
-
+auth.login({identifier: "littlebadbad", password: "123456"})
+    .then(r => {
+        strapiClient.defaults.headers["authorization"] = `Bearer ${r.jwt}`;
+        return user.me({populate: ["image"]})
+    }).then(r => {
     console.log(r);
 })
